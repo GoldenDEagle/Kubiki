74a73c884e003b3e835348e86704aa313cf09e3d
GoldenDEagle <ionufriev95@gmail.com>
2023-08-02 16:17:42 +0600

    added dices

---
+++ Assets/Codebase/Gameplay/General/CanvasVersion/Dice.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Codebase.Gameplay.General.CanvasVersion
+{
+    public class Dice : MonoBehaviour
+    {
+        [SerializeField] private Toggle _diceToggle;
+    }
+}
