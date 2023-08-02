0e3be93b00c6c301a757fa8d700890d3a13678a5
GoldenDEagle <ionufriev95@gmail.com>
2023-08-02 17:28:25 +0600

    general game scene look

---
+++ Assets/Codebase/Gameplay/General/CanvasVersion/CombinationField.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Codebase.Gameplay.General.CanvasVersion
+{
+    public class CombinationField : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _scoreText;
+        [SerializeField] private Image _cross;
+    }
+}
