1ede97e847645fc46c2cb379c8514d4fbefd561e
GoldenDEagle <ionufriev95@gmail.com>
2023-08-04 21:00:54 +0600

    trying to pass turns

--- Assets/Codebase/Gameplay/Multiplayer/MultiplayerGame.cs
+++ Assets/Codebase/Gameplay/Multiplayer/MultiplayerGame.cs
@@ -1,27 +1,73 @@
+using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Codebase.Gameplay.Multiplayer
 {
     [RequireComponent(typeof(PunTurnManager))]
     public class MultiplayerGame : MonoBehaviour, IPunTurnManagerCallbacks
     {
+        [SerializeField] private Button _button;
+
         private PunTurnManager _turnManager;
+        private int _activePlayerId = 0;
 
         private void Awake()
         {
             _turnManager = GetComponent<PunTurnManager>();
+            _turnManager.TurnManagerListener = this;
         }
 
         private void Start()
         {
-            _turnManager.TurnManagerListener = this;
+            BeginNewTurnIfMaster();
         }
 
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(EndPlayerTurn);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveAllListeners();
+        }
+
+        private void BeginNewTurnIfMaster()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                _turnManager.BeginTurn();
+            }
+        }
+
+        private void EndPlayerTurn()
+        {
+            PhotonNetwork.LocalPlayer.SetFinishedTurn(_turnManager.Turn);
+        }
+
+
+
+        // Photon Callbacks
         public void OnPlayerFinished(Player player, int turn, object move)
         {
-            throw new System.NotImplementedException();
+            _activePlayerId++;
+
+            if (_activePlayerId > PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                _activePlayerId = 1;
+            }
+
+            if (PhotonNetwork.LocalPlayer.ActorNumber == _activePlayerId)
+            {
+                _button.gameObject.SetActive(true);
+            }
+            else
+            {
+                _button.gameObject.SetActive(false);
+            }
         }
 
         public void OnPlayerMove(Player player, int turn, object move)
@@ -31,12 +77,23 @@ namespace Assets.Codebase.Gameplay.Multiplayer
 
         public void OnTurnBegins(int turn)
         {
-            throw new System.NotImplementedException();
+            if (PhotonNetwork.LocalPlayer.ActorNumber == _activePlayerId)
+            {
+                _button.gameObject.SetActive(true);
+            }
+            else
+            {
+                _button.gameObject.SetActive(false);
+            }
         }
 
         public void OnTurnCompleted(int turn)
         {
-            throw new System.NotImplementedException();
+            if (_turnManager.Turn == turn)
+            {
+                _activePlayerId = 1;
+                BeginNewTurnIfMaster();
+            }
         }
 
         public void OnTurnTimeEnds(int turn)
